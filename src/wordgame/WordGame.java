8abf965fb47d1d4405b1a3ffc31f6c7e849993d5
cecoenPTC <44152741+cecoenPTC@users.noreply.github.com>
2019-02-12 09:48:10 -0500

    Second commit.

--- src/wordgame/WordGame.java
+++ src/wordgame/WordGame.java
@@ -5,7 +5,18 @@ import java.util.Scanner;
 public class WordGame {
 
     public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String word = "butter";
+        String input;
         
+        System.out.println("Guess the word >> ");
+        input = kb.nextLine();
+        
+        if(input .equals(word)){
+            System.out.println("Correct!");
+        }else{
+            System.out.println("Incorrect!");
+        }
     }
 
 }
