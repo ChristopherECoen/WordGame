03cd0bb3c44acab6783c52931637eb8dc28e351c
christophercoenpersonal@gmail.com <>
2019-02-12 10:07:34 -0500

    Third

--- src/wordgame/WordGame.java
+++ src/wordgame/WordGame.java
@@ -5,16 +5,20 @@ import java.util.Scanner;
 public class WordGame {
 
     public static void main(String[] args) {
+        //Scanner, base word, input 
         Scanner kb = new Scanner(System.in);
-        String word = "butter";
+        String word = "BUTTER";
         String input;
-        
+
+        //Guess word, convert word to uppercase
         System.out.println("Guess the word >> ");
         input = kb.nextLine();
-        
-        if(input .equals(word)){
+        input = input.toUpperCase();
+
+        //if input is the same as word, correct. Else, incorrect.
+        if (input.equals(word)) {
             System.out.println("Correct!");
-        }else{
+        } else {
             System.out.println("Incorrect!");
         }
     }
